57b43ab94d3918222c73cff61bd99a937a6fcf8a
Felipe Pereira De Britto <felipepereiradebritto08@gmail.com>
2024-08-19 20:26:08 -0300

    corrigindo

--- while_else.py
+++ while_else.py
@@ -2,6 +2,23 @@
 # Crie um laço loop while que se repete 
 # enquanto uma determinada condição for verdadeira.
 
+while True:
+    idade_entrada = int(input("Digite a temperatura da água para verificar a temperatura está liguida\nDigite sua opção:"))
+    if idade_entrada >= 100:
+        print("A água está em ebulição\nPrograma encerrado...")
+        break
+    else:
+        print("A água está liquida")
+
+
+while True:
+    primo_input = int(input("Digite um número para verificar se é numero primo\nDigite sua opção:"))
+    if primo_input / primo_input == primo_input:
+        print("O número não é primo")
+        break
+    else:
+        print("Seu número é primo")
+
 # O loop while deve ter um else para 
 # executar quanto o loop while for finalizado 
 # normalmente.
